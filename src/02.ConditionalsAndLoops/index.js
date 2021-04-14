583c4a9a44f71b858ac291cf4d02f49919241895
jypark <jypark@eduhansol.co.kr>
2021-04-14 16:19:30 +0900

    02.ConditionalsAndLoops

---
+++ src/02.ConditionalsAndLoops/index.js
@@ -0,0 +1,19 @@
+var app3 = new Vue({
+
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+})
+
+var app4 = new Vue({
+
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: 'Learn JavaScript'},
+            { text: 'Learn Vue'},
+            { text: 'Build something awesome'},
+        ]
+    }
+})
